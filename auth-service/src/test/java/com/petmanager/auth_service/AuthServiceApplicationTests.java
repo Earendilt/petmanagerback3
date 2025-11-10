b93ea0392987e08227d00e6bb0576a73bdf047cf
Eärendil <andres.lopez2@udea.edu.co>
2025-11-10 16:49:56 -0500

    commit 11

--- auth-service/src/test/java/com/petmanager/auth_service/AuthServiceApplicationTests.java
+++ auth-service/src/test/java/com/petmanager/auth_service/AuthServiceApplicationTests.java
@@ -1,5 +1,5 @@
 package com.petmanager.auth_service;
-
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 @Disabled("Desactivado para evitar error de conexión en CI")
