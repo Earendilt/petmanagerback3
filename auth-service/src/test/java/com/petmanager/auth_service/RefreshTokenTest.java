5b2aff596ad1ee6c864c51b8a6fcd14311c54e11
Eärendil <andres.lopez2@udea.edu.co>
2025-11-10 22:26:34 -0500

    commit 42

---
+++ auth-service/src/test/java/com/petmanager/auth_service/RefreshTokenTest.java
@@ -0,0 +1,25 @@
+package com.petmanager.auth_service.entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.Instant;
+@Entity
+@Table(name = "refresh_token")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RefreshToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String token;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_usuario", nullable = false)
+    private SystemUser user;
+    @Column(name = "fecha_expiracion", nullable = false)
+    private Instant expiryDate;
+}
